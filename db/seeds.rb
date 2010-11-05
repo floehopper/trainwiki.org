836faef44fff8b08a758853fbf0f74706539ab9b
James Mead <james@floehopper.org>
2010-11-05 11:09:55 +0000

    Added station model and seed data using national-rail gem station list.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require "national-rail"
+stations = NationalRail::StationList.new
+stations.each do |name, code, latitude, longitude|
+  Station.create!(:name => name, :code => code, :latitude => latitude, :longitude => longitude)
+end
