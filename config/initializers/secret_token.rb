9467dbf781e64fd031c4dbef6a9245b1fa1f30e1
James Mead <james@floehopper.org>
2010-11-05 10:56:29 +0000

    Fresh Rails 3.0.1 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trainwiki::Application.config.secret_token = 'a44107d7c076d93570e0e38349ecdff0312cc6a74160a679990c9bb4ad2020159bd98bfb837d3c969748523f2cd1f3698fe02cc37191af134a852a3fb6089dfd'
