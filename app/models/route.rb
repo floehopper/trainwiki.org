9a700a2391daa3f336adb471bffb12324f7a6353
James Mead <james@floehopper.org>
2010-11-19 11:41:43 +0000

    Extract route-specific scraping code into a method on Route.

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,64 @@ class Route < ActiveRecord::Base
   belongs_to :line
   belongs_to :origin_station, :class_name => "Station"
   belongs_to :destination_station, :class_name => "Station"
+
+  def scrape_journeys(start_time, delay_average = 2, delay_variation = 2)
+    origin = origin_station.name
+    destination = destination_station.name
+    time = start_time
+    finished = false
+    while !finished
+      planner = NationalRail::JourneyPlanner.new
+      puts "#{time.to_s(:short)} - search for journeys from #{origin} to #{destination}"
+
+      summary_rows = planner.plan(:from => origin, :to => destination, :time => time)
+      summary_rows.each do |summary_row|
+        timestamp = summary_row.departure_time.to_s(:short)
+
+        unless summary_row.departure_time > time
+          puts "#{timestamp} - skipped because departure time is earlier than the search time"
+          next
+        end
+        unless summary_row.departure_time.to_date == time.to_date
+          puts "#{timestamp} - aborting because departure time is not on the same day"
+          finished = true
+          break
+        end
+        unless summary_row.number_of_changes == "0"
+          puts "#{timestamp} - skipped because it has #{summary_row.number_of_changes} changes"
+          next
+        end
+
+        details = summary_row.details
+
+        company = details[:company]
+        unless company == line.name
+          puts "#{timestamp} - skipped because journey is not an #{line.name} service"
+          next
+        end
+
+        origins, destinations = details[:origins], details[:destinations]
+        unless origins.include?(origin) && destinations.include?(destination)
+          puts "#{timestamp} - skipped because journey is from #{origins.join(",")} to #{destinations.join(",")} (not from #{origin} to #{destination})"
+          next
+        end
+        puts "#{timestamp} --->>> departure with #{details[:stops].length} stops found"
+
+        journey = Journey.build_from(details)
+
+        unless journey.save
+          puts "#{timestamp} - journey not valid: #{journey.errors.full_messages.join(", ")}"
+        end
+
+        GC.start
+        sleep(delay_average + (delay_variation * 2) * (rand - 0.5))
+      end
+
+      if summary_rows.empty?
+        time += 1.minute
+      else
+        time = summary_rows.last.departure_time + 1.minute
+      end
+    end
+  end
 end
--- lib/timetable_scraper.rb
+++ lib/timetable_scraper.rb
@@ -5,63 +5,7 @@ class TimetableScraper
   def scrape(line_name, start_time, delay_average = 2, delay_variation = 2)
     line = Line.find_by_name!(line_name)
     line.routes.each do |route|
-      origin = route.origin_station.name
-      destination = route.destination_station.name
-      time = start_time
-      finished = false
-      while !finished
-        planner = NationalRail::JourneyPlanner.new
-        puts "#{time.to_s(:short)} - search for journeys from #{origin} to #{destination}"
-
-        summary_rows = planner.plan(:from => origin, :to => destination, :time => time)
-        summary_rows.each do |summary_row|
-          timestamp = summary_row.departure_time.to_s(:short)
-
-          unless summary_row.departure_time > time
-            puts "#{timestamp} - skipped because departure time is earlier than the search time"
-            next
-          end
-          unless summary_row.departure_time.to_date == time.to_date
-            puts "#{timestamp} - aborting because departure time is not on the same day"
-            finished = true
-            break
-          end
-          unless summary_row.number_of_changes == "0"
-            puts "#{timestamp} - skipped because it has #{summary_row.number_of_changes} changes"
-            next
-          end
-
-          details = summary_row.details
-
-          company = details[:company]
-          unless company == line.name
-            puts "#{timestamp} - skipped because journey is not an #{line.name} service"
-            next
-          end
-
-          origins, destinations = details[:origins], details[:destinations]
-          unless origins.include?(origin) && destinations.include?(destination)
-            puts "#{timestamp} - skipped because journey is from #{origins.join(",")} to #{destinations.join(",")} (not from #{origin} to #{destination})"
-            next
-          end
-          puts "#{timestamp} --->>> departure with #{details[:stops].length} stops found"
-
-          journey = Journey.build_from(details)
-
-          unless journey.save
-            puts "#{timestamp} - journey not valid: #{journey.errors.full_messages.join(", ")}"
-          end
-
-          GC.start
-          sleep(delay_average + (delay_variation * 2) * (rand - 0.5))
-        end
-
-        if summary_rows.empty?
-          time += 1.minute
-        else
-          time = summary_rows.last.departure_time + 1.minute
-        end
-      end
+      route.scrape_journeys(start_time, delay_average, delay_variation)
     end
   end
 end
