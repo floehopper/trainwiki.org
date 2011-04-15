f749002638f55083c6eb1e88cf1df299b43746c5
James Mead <james@floehopper.org>
2011-04-15 16:52:49 +0100

    No need to query for journeys on previous and next days multiple times on the index page.
    Also use day of the week as friendlier navigation link text.

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -4,6 +4,8 @@ class JourneysController < ApplicationController
     if params[:date].present?
       @date = Date.parse(params[:date])
       @journeys = Journey.departs_on(@date).includes(:origin_station, :destination_station).order(:departs_at)
+      @previous_journeys = Journey.departs_on(@date - 1).any?
+      @next_journeys = Journey.departs_on(@date + 1).any?
     else
       redirect_to journeys_path(:date => Date.today)
     end
