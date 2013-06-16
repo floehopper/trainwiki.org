e9d9a0d685346f76f93235539e2f8eddd6998117
James Mead <james@floehopper.org>
2013-06-16 10:29:52 +0100

    Disable timetable:scrape cron job.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ env :PATH, '/usr/bin:/bin:/usr/local/bin'
 env :DELAY_AVERAGE, 5
 env :DELAY_VARIATION, 3
 
-every 1.day, :at => '2:00am' do
-  rake 'timetable:scrape'
-end
+# every 1.day, :at => '2:00am' do
+#   rake 'timetable:scrape'
+# end
